bf7580cd4a2eff841b5c965a4617b7f65bd2f8b9
rajkumar191512018 <40802906+rajkumar191512018@users.noreply.github.com>
2018-07-03 23:14:15 +0530

    Create v.py

---
+++ v.py
@@ -0,0 +1,7 @@
+a=input()
+if a in ('a','e','i','o','u'):
+  print("vowels")
+  elif a in ('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','x','y','z'):
+  print("consonant")
+else:
+print("invalid")
